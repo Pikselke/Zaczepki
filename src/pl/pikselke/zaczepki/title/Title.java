6e9fa066c6e93140261023e717d3597a86d47556
Pikselke <piksel.biznes@wp.pl>
2017-10-03 19:00:21 +0200

    Update Title.java

--- src/pl/pikselke/zaczepki/title/Title.java
+++ src/pl/pikselke/zaczepki/title/Title.java
@@ -12,8 +12,8 @@ import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
 public class Title implements Listener{
 	
 	public static void sendTitle(Player p, String title){
-        PacketPlayOutTitle packet = new PacketPlayOutTitle(EnumTitleAction.TITLE, ChatSerializer.a("{\"text\":\"" + title + "\"}"), 1, 2, 1);
-        ((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
+        	PacketPlayOutTitle packet = new PacketPlayOutTitle(EnumTitleAction.TITLE, ChatSerializer.a("{\"text\":\"" + title + "\"}"), 1, 2, 1);
+        	((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
 	}
 	
 }
